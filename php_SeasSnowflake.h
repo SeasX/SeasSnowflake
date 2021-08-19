f519ab26117cd3344d08f42129385449e84fd615
Rock <35133768+Rock-liyi@users.noreply.github.com>
2021-08-19 15:19:42 +0800

    Update php_SeasSnowflake.h

--- php_SeasSnowflake.h
+++ php_SeasSnowflake.h
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author:  SeasX Group <ahhhh.wang@gmail.com>                          |
+  | Author:  SeasX Group <rock@php.net>                          |
   +----------------------------------------------------------------------+
 */
 #ifdef __cplusplus
